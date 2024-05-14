3a8819b908761562b9f73dfa7a5f8c1586003f6f
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-05-14 21:26:00 +0900

    feat: impl some functions

--- main.ts
+++ main.ts
@@ -1,14 +1,47 @@
+import {} from "npm:@types/google-apps-script";
+
 declare let global: {
   doGet: (e?: GoogleAppsScript.Events.DoGet) =>
-    | GoogleAppsScript.HTML.HtmlOutput
-    | GoogleAppsScript.Content.TextOutput;
+    | GoogleAppsScript.HTML.HtmlOutput | GoogleAppsScript.Content.TextOutput;
   doPost: (e?: GoogleAppsScript.Events.DoPost) =>
-    | GoogleAppsScript.HTML.HtmlOutput
-    | GoogleAppsScript.Content.TextOutput;
+    | GoogleAppsScript.HTML.HtmlOutput | GoogleAppsScript.Content.TextOutput;
   [key: string]: () => void;
+  getCourseList: () => GoogleAppsScript.Classroom.Schema.Course[] | undefined;
+  getCourseIds: () => (string | undefined)[] | undefined;
+  getAssignments: () => (GoogleAppsScript.Classroom.Schema.ListCourseWorkResponse | undefined)[] | undefined;
+  sanitizeUndefined: (x: unknown[]) => unknown[];
 };
 
-global.createNewSpreadsheet = () => {
-  const spreadsheet = SpreadsheetApp.create("New file");
-  spreadsheet.getRange("A1").setValue("Hello World!");
-};
+global.sanitizeUndefined = (x: unknown[]) =>  {
+  return x.filter((x) => x !== undefined);
+}
+
+global.getCourseList = () => {
+  const courses = Classroom.Courses;
+
+  if (courses === undefined) return undefined;
+
+  const list = courses.list().courses;
+  return list;
+}
+
+global.getCourseIds = () => {
+  const courseList = global.getCourseList();
+
+  if (courseList === undefined) return undefined;
+
+  const ids = courseList.map((x) => x.id);
+  return ids
+}
+
+global.getAssignments = () => {
+  const ids = global.getCourseIds();
+  if (ids === undefined) return undefined;
+
+  const assignments = ids.map((id) => {
+    if (id === undefined || Classroom.Courses === undefined || Classroom.Courses.CourseWork === undefined) return undefined;
+    return Classroom.Courses.CourseWork.list(id);
+  })
+
+  return assignments;
+}
