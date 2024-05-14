1e283e434f3c4b2a9dd4a0202ab9feb6c7a94830
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-05-15 01:22:01 +0900

    refactor: sanitize undefined

--- main.ts
+++ main.ts
@@ -1,20 +1,18 @@
 import {} from "npm:@types/google-apps-script";
 
+type Course = GoogleAppsScript.Classroom.Schema.Course;
+type ListCourseWorkResponse = GoogleAppsScript.Classroom.Schema.ListCourseWorkResponse;
+
 declare let global: {
   doGet: (e?: GoogleAppsScript.Events.DoGet) =>
     | GoogleAppsScript.HTML.HtmlOutput | GoogleAppsScript.Content.TextOutput;
   doPost: (e?: GoogleAppsScript.Events.DoPost) =>
     | GoogleAppsScript.HTML.HtmlOutput | GoogleAppsScript.Content.TextOutput;
   [key: string]: () => void;
-  getCourseList: () => GoogleAppsScript.Classroom.Schema.Course[] | undefined;
-  getCourseIds: () => (string | undefined)[] | undefined;
-  getAssignments: () => (GoogleAppsScript.Classroom.Schema.ListCourseWorkResponse | undefined)[] | undefined;
-  sanitizeUndefined: (x: unknown[]) => unknown[];
+  getCourseList: () => Course[] | undefined;
+  getCourseIds: () => string[] | undefined;
+  getAssignments: () => ListCourseWorkResponse[] | undefined;
 };
-
-global.sanitizeUndefined = (x: unknown[]) =>  {
-  return x.filter((x) => x !== undefined);
-}
 
 global.getCourseList = () => {
   const courses = Classroom.Courses;
@@ -30,18 +28,19 @@ global.getCourseIds = () => {
 
   if (courseList === undefined) return undefined;
 
-  const ids = courseList.map((x) => x.id);
+  const ids = courseList.map((x) => x.id).filter((x): x is string => x !== undefined);
   return ids
 }
 
 global.getAssignments = () => {
   const ids = global.getCourseIds();
+
   if (ids === undefined) return undefined;
 
   const assignments = ids.map((id) => {
     if (id === undefined || Classroom.Courses === undefined || Classroom.Courses.CourseWork === undefined) return undefined;
     return Classroom.Courses.CourseWork.list(id);
-  })
+  }).filter((x): x is ListCourseWorkResponse => x !== undefined);
 
   return assignments;
 }
