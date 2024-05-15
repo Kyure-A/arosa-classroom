386ec36f50081698750b9112b3473559e87f02db
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-05-15 19:03:45 +0900

    feat: add feature to filter assignment

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import {} from "npm:@types/google-apps-script";
 
 type Course = GoogleAppsScript.Classroom.Schema.Course;
-type ListCourseWorkResponse = GoogleAppsScript.Classroom.Schema.ListCourseWorkResponse;
+type CourseWork = GoogleAppsScript.Classroom.Schema.CourseWork;
 
 declare let global: {
   doGet: (e?: GoogleAppsScript.Events.DoGet) =>
@@ -11,7 +11,8 @@ declare let global: {
   [key: string]: () => void;
   getCourseList: () => Course[] | undefined;
   getCourseIds: () => string[] | undefined;
-  getAssignments: () => ListCourseWorkResponse[] | undefined;
+  getAssignments: () => CourseWork[] | undefined;
+  filterAssignments: (date: Date) => CourseWork[] | undefined;
 };
 
 global.getCourseList = () => {
@@ -39,8 +40,30 @@ global.getAssignments = () => {
 
   const assignments = ids.map((id) => {
     if (id === undefined || Classroom.Courses === undefined || Classroom.Courses.CourseWork === undefined) return undefined;
-    return Classroom.Courses.CourseWork.list(id);
-  }).filter((x): x is ListCourseWorkResponse => x !== undefined);
+    return Classroom.Courses.CourseWork.list(id).courseWork;
+  }).filter((x): x is CourseWork[] => x !== undefined).flat();
+
+  return assignments;
+}
+
+global.filterAssignments = (date: Date) => {
+  const allAssignments = global.getAssignments();
+
+  if (allAssignments === undefined) return undefined;
+
+  const assignments = allAssignments.filter((x): x is CourseWork => {
+    const d = x.dueDate;
+    const t = x.dueTime;
+
+    if (d === undefined || t === undefined) return false;
+    if (d.year === undefined || d.month === undefined || d.day === undefined) return false;
+    if (t.hours === undefined || t.minutes === undefined || t.seconds === undefined) return false;
+
+    const assignmentsDate = new Date(d.year, d.month, d.day, t.hours, t.minutes, t.seconds);
+    if (assignmentsDate < date) return false;
+
+    return true;
+  })
 
   return assignments;
 }
