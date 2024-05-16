b1678e4b9223a37a4985497f5ba83dbeaa8bf5c2
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-05-16 22:48:28 +0900

    feat: impl `filterAssignments()`, `doGet()`

--- main.ts
+++ main.ts
@@ -39,24 +39,84 @@ global.getAssignments = () => {
   if (ids === undefined) return undefined;
 
   const assignments = ids.map((id) => {
-    if (id === undefined || Classroom.Courses === undefined || Classroom.Courses.CourseWork === undefined) return undefined;
+    if (
+      id === undefined || Classroom.Courses === undefined ||
+      Classroom.Courses.CourseWork === undefined
+    ) return undefined;
     return Classroom.Courses.CourseWork.list(id).courseWork;
   }).filter((x): x is CourseWork[] => x !== undefined).flat();
 
   return assignments;
-}
+};
 
 global.filterAssignmentsByDate = (allAssignments: CourseWork[], date: Date) => {
   const assignments = allAssignments.filter((x): x is CourseWork => {
     const d = x.dueDate;
 
-    if (d === undefined || d.year === undefined || d.month === undefined || d.day === undefined) return false;
+    if (
+      d === undefined || d.year === undefined || d.month === undefined ||
+      d.day === undefined
+    ) return false;
 
     const assignmentsDate = new Date(d.year, d.month, d.day);
     if (assignmentsDate < date) return false;
 
     return true;
-  })
+  });
 
   return assignments;
-}
+};
+
+global.formatAssignments = (assignments: CourseWork[]): Formatted[] => {
+  const formatted = assignments.map((x) => {
+    return {
+      name: x.title !== undefined ? x.title : "",
+      description: x.description !== undefined ? x.description : "",
+      dueDate: x.dueDate !== undefined
+        ? {
+          year: x.dueDate.year ?? 1970,
+          month: x.dueDate.month ?? 1,
+          day: x.dueDate.day ?? 1,
+        }
+        : undefined,
+      link: x.alternateLink !== undefined ? x.alternateLink : "",
+    } satisfies Formatted;
+  });
+
+  return formatted;
+};
+
+global.doGet = (e?) => {
+  if (e === undefined) {
+    return ContentService.createTextOutput().setContent(
+      JSON.stringify({ status: "error", value: undefined }),
+    );
+  }
+
+  const assignments = global.getAssignments();
+  if (assignments === undefined) {
+    return ContentService.createTextOutput().setContent(
+      JSON.stringify({ status: "error", value: undefined }),
+    );
+  }
+
+  const filterAssignments = global.filterAssignmentsByDate(
+    assignments,
+    new Date(
+      Number(e.parameters.year),
+      Number(e.parameters.month),
+      Number(e.parameters.day),
+    ),
+  );
+
+  if (filterAssignments === undefined) {
+    return ContentService.createTextOutput().setContent(
+      JSON.stringify({ status: "error", value: undefined }),
+    );
+  }
+  const formatted = global.formatAssignments(filterAssignments);
+
+  return ContentService.createTextOutput().setContent(
+    JSON.stringify({ status: "ok", value: formatted }),
+  );
+};
